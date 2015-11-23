35dd25dbe0250b47c605738088228aba7d6c7a0d
731mat <matej.housek@seznam.cz>
2015-11-23 17:39:42 +0100

    opoznámkováno
    kód jsem zjednoduššil a přidal jsem pár poznámek k kódu

--- ziskani_kurzu_men_online/src/ziskani_kurzu_men_online/Ziskani_kurzu_men_online.java
+++ ziskani_kurzu_men_online/src/ziskani_kurzu_men_online/Ziskani_kurzu_men_online.java
@@ -1,27 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ziskani_kurzu_men_online;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
-
 import java.net.URL;
 import java.util.ArrayList;
-
-import static sun.org.mozilla.javascript.internal.Token.XML;
-
 
 /**
  *
  * @author Matěj
  */
 public class Ziskani_kurzu_men_online {
-    private static Object phrase;
 
     /**
      * @param args the command line arguments
@@ -30,32 +19,40 @@ public class Ziskani_kurzu_men_online {
         // TODO code application logic here
         String xml = "";
         try {
+            //url adresa pro xml data - kdybych chtěl přidat nějaký kurz měny tak do kulatých závorek
             URL convert = new URL("http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20(%22USDCZK%22,%22EURCZK%22)&env=store://datatables.org/alltableswithkeys");
-            BufferedReader in = new BufferedReader(new InputStreamReader(convert.openStream()));
-            
-            String line="";
-            while ((line = in.readLine()) != null) 
-                xml += line;
+            //načte data z seteamu a uloží do bufferu
+            try (BufferedReader in = new BufferedReader(new InputStreamReader(convert.openStream()))) { // try kdyby se náhodou nepřipojil k internetu
+                String line;
+                while (( line= in.readLine()) != null) { // projde buffer dokud nebude null
+                    xml += line;                           // pridání do string
+                }
+            }
+        }catch (IOException ioe) {
+            System.out.println(ioe); //ošetření chyb při try
+        }
 
-            in.close();
-            System.out.println(xml);
-        } catch (IOException ioe) {
-            System.out.println(ioe);
-        }
-        String delims = "[<>]+";
-        String[] tokens = xml.split(delims);
-        ArrayList<String> l = new ArrayList<String>();
+        // <rate id="EURCZK">
+        // <Name>EUR/CZK</Name>
+        // <Rate>27.0259</Rate>
+        // <Date>11/23/2015</Date>
+        // <Time>3:51pm</Time>
+        // <Ask>27.0340</Ask>
+        // <Bid>27.0179</Bid>
+        // </rate>
+        // </results>
         
-        
-        for (int i = 0; i < tokens.length; i++){
-            if(tokens[i].equals("USD/CZK")){ l.add(tokens[i]);}
-            if(tokens[i].equals("CZK/EUR")){ l.add(tokens[i]);}
-            if(tokens[i].equals("Time")){ l.add(tokens[i+1]);}
-            if(tokens[i].equals("Ask")){ l.add(tokens[i+1]);}
-            //System.out.println(tokens[i]);
+        String oddelovace = "[<>]+";
+        String[] radky = xml.split(oddelovace);  //rozparsování řetězce
+        ArrayList<String> l = new ArrayList<>(); //vytvoření listu kde pole jsou string 
+        String[] znacky = {"Name","Date", "Time", "Ask"}; //názvy hodnot keré chceme uložit 
+        for (int i = 0; i < radky.length; i++) {  // projde rozparsovaný xml soubor bez <>
+            for (String s : znacky) {               // projde arry list 
+                if (radky[i].equals(s)) {           // zjistí zkusí zda string ze značek neodpovídá stringu z rozparsovaného xml
+                    l.add(radky[i + 1]);            // když ano tak přidá položkudo pole
+                }
+            }
         }
-        System.out.println(l);
+        System.out.println(l); //výpis listu
     }
-
-
 }
