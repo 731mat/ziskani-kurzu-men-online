7a8b397f20e7473f94dd236113347b62f7fb976d
731mat <matej.housek@seznam.cz>
2015-11-23 15:57:06 +0100

    uložení hodnot do listu

--- ziskani_kurzu_men_online/src/ziskani_kurzu_men_online/Ziskani_kurzu_men_online.java
+++ ziskani_kurzu_men_online/src/ziskani_kurzu_men_online/Ziskani_kurzu_men_online.java
@@ -5,17 +5,57 @@
  */
 package ziskani_kurzu_men_online;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+import java.net.URL;
+import java.util.ArrayList;
+
+import static sun.org.mozilla.javascript.internal.Token.XML;
+
+
 /**
  *
  * @author Matěj
  */
 public class Ziskani_kurzu_men_online {
+    private static Object phrase;
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        String xml = "";
+        try {
+            URL convert = new URL("http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20(%22USDCZK%22,%22EURCZK%22)&env=store://datatables.org/alltableswithkeys");
+            BufferedReader in = new BufferedReader(new InputStreamReader(convert.openStream()));
+            
+            String line="";
+            while ((line = in.readLine()) != null) 
+                xml += line;
+
+            in.close();
+            System.out.println(xml);
+        } catch (IOException ioe) {
+            System.out.println(ioe);
+        }
+        String delims = "[<>]+";
+        String[] tokens = xml.split(delims);
+        ArrayList<String> l = new ArrayList<String>();
+        
+        
+        for (int i = 0; i < tokens.length; i++){
+            if(tokens[i].equals("USD/CZK")){ l.add(tokens[i]);}
+            if(tokens[i].equals("CZK/EUR")){ l.add(tokens[i]);}
+            if(tokens[i].equals("Time")){ l.add(tokens[i+1]);}
+            if(tokens[i].equals("Ask")){ l.add(tokens[i+1]);}
+            //System.out.println(tokens[i]);
+        }
+        System.out.println(l);
     }
-    
+
+
 }
